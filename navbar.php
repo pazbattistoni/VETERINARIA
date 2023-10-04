9826fca69952cf5c95dfe24092f2328fd080439a
Camila <camilasalvia2@gmail.com>
2023-10-04 14:59:03 -0300

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Veterinaria San Antón</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+  <nav class="navbar navbar-expand-md bg-body-tertiary navbar-static-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Veterinaria San Antón</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        <span class="visually-hidden">Desplegar la barra de navegación</span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+          <a class="nav-link" href="#">Nosotros</a>
+          <a class="nav-link" href="#">Servicios</a>
+          <a class="nav-link" href="#">Consultas a domicilio</a>
+          <a class="nav-link" href="#">Hotelería</a>
+          <a class="nav-link" href="#">Contacto</a>
+        </div>
+        <div class="navbar-nav">
+          <a class="nav-link" href="#">Ingresar</a>
+          <a class="nav-link" href="#">Registrarse</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <?php
+    include("login.php");
+  ?>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
