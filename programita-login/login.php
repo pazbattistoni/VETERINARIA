ff3dcbda2ac7fd3c7ceeac0e48283998edcbacf1
Camila <camilasalvia2@gmail.com>
2023-10-09 00:11:18 -0300

    Update login.php

--- programita-login/login.php
+++ programita-login/login.php
@@ -2,10 +2,12 @@
   include("conexion_db.php");
   $USUARIO= $_POST['usuario'];
   $PASSWORD= $_POST['password'];
+  session_start();
+  $_SESION['usuario']=$USUARIO;
   $consulta= "SELECT* FROM personal where usuario='$USUARIO' and password='$PASSWORD'";
   $resultado= mysqli_query($connection, $consulta);
-  $filas= mysqli_num_rows($resultado);
-  if($filas){
+  $filas= mysqli_fetch_array($resultado);
+  if($filas['id_cargo']==1){ //administrador
     header("location:home.html");
   } else{
     include("index.html");
